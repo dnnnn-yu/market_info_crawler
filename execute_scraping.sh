6fae257b2a4e5fffc3a3ac7e58783bd485ce3376
yuta.dammatsu <yuta.dammatsu@rooter.co.jp>
2020-03-25 12:16:56 +0900

    各クローラーを実行するシェルスクリプトを作成

---
+++ execute_scraping.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+bundle exec ruby ./market_info_crawler/index.rb
+sleep 10s
+bundle exec ruby ./market_info_crawler/volume_ranking.rb
+sleep 10s
+bundle exec ruby ./market_info_crawler/increase_ranking.rb
+sleep 10s
+bundle exec ruby ./market_info_crawler/decrease_ranking.rb
+sleep 10s
+bundle exec ruby ./market_info_crawler/pickup.rb
+
